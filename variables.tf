8ff688a202f28822283f942fa544d94227fb2122
gensdumonde <gcorpuz@gmail.com>
2022-04-01 11:36:27 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -218,3 +218,6 @@ variable "protect_from_scale_in" {
   description = "Allows setting instance protection. The autoscaling group will not select instances with this setting for terminination during scale in events."
   default     = false
 }
+
+variable access_key{}
+variable secret_key {}
